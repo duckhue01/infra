3e03aedc8fe99ba25999f1d3c753227e332ad41f
Duc Khue <duckhue01.tech@gmail.com>
2026-03-18 15:20:43 +0700

    feat: init

--- aws/elb/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "bucket_arn" {
-  description = "ARN of the demo S3 bucket"
-  value       = aws_s3_bucket.demo.arn
-}
--- aws/elb/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "bucket_name" {
-  description = "Name for the example S3 bucket"
-  type        = string
-  default     = "lab-demo-bucket-123456"
-}
